9e1b30f8f511e466892113c03b10439ff32ac130
Resep2 <Resep2@172.17.7.121>
2020-08-02 14:19:59 +0300

    add some things

--- src/com/valentinet/StringUtil.java
+++ src/com/valentinet/StringUtil.java
@@ -1,9 +1,47 @@
+
 package com.valentinet;
+
+import java.util.Scanner;
 
 public class StringUtil {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		Scanner in = new Scanner(System.in);
+		System.out.print("Введите фразу : ");
+		String source = in.nextLine();
+
+//		System.out.print("Ваша фраза: ");
+//		System.out.println(source);
+		in.close();
+
+//		String source = "Hello my World!";
+
+		String[] words = source.split(" ");
+
+		System.out.println("Количество слов : " + words.length);
+
+		for (int i = 0; i < words.length; i++) {
+
+			System.out.print(words[i] + ": ");
+
+			char[] letters1 = words[i].toCharArray();
+
+			System.out.println("Количество символов : " + letters1.length);
+
+			// char[][] letters = { letters1, letters2 };
+
+		}
+
+		// char[] letters1 = words[0].toCharArray();
+		// char[] letters2 = words[1].toCharArray();
+
+		// char[][] letters = { letters1, letters2 };
+		// System.out.println(letters2.length);
+
+//		System.out.println(words[0]);
+//		System.out.println(words[1]);
+//		System.out.println(letters[0][0]);
 
 	}
 
